9cd76cba7bb80dd21d163961058c83bba5815258
soroush <soroushsalamati94@gmail.com>
2024-08-28 21:40:24 +0330

    '1'

--- shopping/models.py
+++ shopping/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
-
+    description = models.TextField()
     def __str__(self):
         return self.name
 
---
+++ shopping/views.py
@@ -0,0 +1,20 @@
+from django.http.response import HttpResponse , JsonResponse
+from shopping.models import Customer , Order , Product ,Factor, Category
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.shortcuts import render
+
+
+def product_list(request):
+    products = Product.objects.all()
+    my_product_list = []
+    for item in products:
+        product_dictionary = {
+            "name" : item.name,
+            "description" : item.description,
+            "price" : item,
+            "category" : item.category.name
+        }
+        my_product_list.append(product_dictionary)
+    return JsonResponse(my_product_list, safe=False)
+
